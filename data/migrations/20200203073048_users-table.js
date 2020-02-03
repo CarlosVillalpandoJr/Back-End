e26b704b46f692d8d7bf6ea33b10c2a2f4d19217
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-02-03 07:50:32 -0600

    added user table

---
+++ data/migrations/20200203073048_users-table.js
@@ -0,0 +1,33 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+
+        users
+            .string('username')
+            .notNullable()
+            .unique()
+
+        users
+            .string('password')
+            .notNullable()
+            .unique();
+
+        users
+            .string('name')
+            .notNullable()
+
+        users
+            .string('city')
+            .notNullable()
+
+        users
+            .string('email')
+            .notNullable()
+            .unique();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('users')
+};
