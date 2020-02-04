e5c9ebd9fe57d0dc4000a876d04cbd5af73de911
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-02-04 09:02:16 -0600

    added get user restaurants by id

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const server = express();
 const AuthRouter = require('../auth/auth-router');
 const UserRouter = require('../users/users-router');
 const CitiesRouter = require('../cities/cities-router');
+const UserRestRouter = require('../user-rest/userRest-router');
 
 server.use(helmet());
 server.use(express.json());
@@ -17,6 +18,7 @@ server.use(cors());
 server.use('/api/auth', AuthRouter);
 server.use('/api/users', UserRouter);
 server.use('/api/cities', CitiesRouter);
+server.use('/api/userrest', UserRestRouter);
 
 server.get('/', (req, res) => {
     res.send("API Running")
---
+++ user-rest/userRest-router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const UserRest = require('./userRest-model.js');
+
+const router = express.Router();
+
+// get error
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    UserRest.getUserRest(id)
+        .then(clicked => {
+            res.status(200).json(clicked)
+        })
+        .catch(error => {
+            console.log('get user rest', error)
+            res.status(500).json(error)
+        });
+});
+
+module.exports = router;
